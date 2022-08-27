d2af16009247abb12082f66f64ae4227da467853
bsse1017kavi <bsse1017@iit.du.ac.bd>
2022-08-27 21:52:26 +0600

    Implemented static analysis using cryptoguard and cognicrypt

--- playground/urls.py
+++ playground/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.simple_upload, name='home'),
-    path("select", views.select_tool, name='select')
+    path("select", views.select_tool, name='select'),
+    path("result", views.result, name="result"),
+    path("test1", views.test1, name="test1"),
+    path("test2", views.test2, name="test2")
     #  path('hello', views.select_tool)
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+import subprocess
+import os
+from django import forms
+
+name = 'hello'
+
+def run_source():
+    os.environ['ANDROID_HOME'] = '/home/ahmedulkavi/Android/Sdk'
+    os.environ['JAVA7_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
+    os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
+
+def run_cognicrypt():
+    cmd = "java"
+
+    cognicrypt_path = '/home/ahmedulkavi/Desktop/djangoProjects/securityTool/cryptoTools/CryptoAnalysis-Android-1.0/CryptoAnalysis-Android-1.0/CryptoAnalysis-Android-1.0.0-jar-with-dependencies.jar'
+    apk = '/home/ahmedulkavi/Desktop/avg.vpn_2.24.5873.apk'
+    android_sdk_platform = '/home/ahmedulkavi/Android/Sdk/platforms'
+    crypto_api_rules = '/home/ahmedulkavi/Desktop/djangoProjects/securityTool/cryptoTools/crypto-api-rules'
+
+    temp = subprocess.Popen([cmd, '-cp', cognicrypt_path, 'main.CogniCryptAndroid', apk, android_sdk_platform, crypto_api_rules, 'report'])
+    temp.communicate()
+
+    # temp = subprocess.Popen([cmd], stdout = subprocess.PIPE)
+    # subprocess.Popen(["cd", android_sdk_platform], stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).communicate()
+    # output = subprocess.Popen(["ls", android_sdk_platform], stdout = subprocess.PIPE).communicate()
+
+def run_cryptoguard():
+    cmd = "java"
+
+    # cryptoguard_source_path = "/home/ahmedulkavi/Desktop/djangoProjects/securityTool/cryptoTools/cryptoguard/_cryptoguard.source"
+
+    cryptoguard_path = '/home/ahmedulkavi/Desktop/djangoProjects/securityTool/cryptoTools/cryptoguard/cryptoguard.jar'
+    apk_path = '/home/ahmedulkavi/Desktop/avg.vpn_2.24.5873.apk'
+
+    run_source()
+
+    temp = subprocess.Popen([cmd, '-jar', cryptoguard_path, '-in', 'apk', '-s', apk_path, '-m', 'L'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    output = temp.communicate()
+    # output = temp.args
+    # output = subprocess.list2cmdline(output)
+
+    return output
 
 # Create your views here.
 def say_hello(request):
@@ -28,3 +70,41 @@ def select_tool(request):
             'uploaded_file_url': uploaded_file_url
         })
     return render(request, 'selector.html')
+
+def result (request):
+    # run_cognicrypt()
+
+    # temp = subprocess.Popen(['java --version'],stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    #output = subprocess.Popen(["java -version"], stdout = subprocess.PIPE).communicate()
+    # output = temp.communicate()
+
+    # return HttpResponse("Report from Cognicrypt generated")
+
+    #output = run_cryptoguard()
+
+    return HttpResponse()
+
+def test1(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            name = "hi"
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/test2', {'name': name})
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'test1.html', {'form': form})
+
+def test2(request):
+    return render(request, 'test2.html', {'name': name})
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
