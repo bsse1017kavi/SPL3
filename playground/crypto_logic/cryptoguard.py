161d4d9546f0028a81ee6088899d30b17b2010be
bsse1017kavi <bsse1017@iit.du.ac.bd>
2022-09-15 22:00:08 +0600

    Refactored Cryptoguard class

---
+++ playground/crypto_logic/cryptoguard.py
@@ -0,0 +1,125 @@
+import os, subprocess, re
+from math import e
+
+class Cryptoguard:
+
+    def run_source(self):
+        os.environ['ANDROID_HOME'] = '/home/ahmedulkavi/Android/Sdk'
+        os.environ['JAVA7_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
+        os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
+
+    def run_cryptoguard(self, file_path):
+        cmd = "java"
+
+        cryptoguard_path = 'crypto_tools/cryptoguard.jar'
+        apk_path = file_path
+
+        self.run_source()
+
+        temp = subprocess.Popen([cmd, '-jar', cryptoguard_path, '-in', 'apk', '-s', apk_path, '-m', 'L'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        output = temp.communicate()
+        # output = temp.args
+        # output = subprocess.list2cmdline(output)
+
+        print('it ran')
+
+        return output
+
+    def check_cryptoguard_violations(self, report):
+        low_weight = 1
+        medium_weight = 2
+        high_weight = 3
+
+        non_standardised_score = 0
+
+        total_violations = report.count('***Violated Rule ')
+
+        res = [m.start() for m in re.finditer(re.escape('***Violated Rule '), report)]
+        violation_count = []
+        violation_rule_number = []
+        summary_list = []
+        summary = ""
+        # report.count('***Violated Rule '),
+        i = 0
+        count = 0
+        for elem in res:
+            if i+1<len(res):
+                count = report.count('***Found:', elem, res[i+1])
+            else:
+                count = report.count('***Found:', elem)
+
+            violation_count.append(count)
+            i+=1
+            
+        for i in range(0, len(violation_count)-1):
+            if violation_count[i] == 0:
+                violation_count[i] = 1
+
+        for elem in res:
+            i = elem + 17
+            num = ""
+            while(report[i]!=':'):
+                num += report[i]
+                i+=1
+
+            num = int(num)
+            violation_rule_number.append(num)
+
+        cnt = 0
+        for elem in res:
+            risk = ''
+            if (violation_rule_number[cnt]>=1 and violation_rule_number[cnt]<=7) or violation_rule_number[cnt]==16:
+                risk = "High"
+                non_standardised_score += high_weight * violation_count[cnt]
+
+            elif violation_rule_number[cnt]>=8 and violation_rule_number[cnt]<=12:
+                risk = "Medium"
+                non_standardised_score += medium_weight * violation_count[cnt]
+
+            else:
+                risk = "Low"
+                non_standardised_score += low_weight * violation_count[cnt]
+
+            if risk=="High":
+                summary += "<div style=\"color:red;\">"
+            elif risk=="Medium":
+                summary += "<div style=\"color:orange;\">"
+            else:
+                summary += "<div style=\"color:black;\">"
+
+            i = elem + 3
+            while(report[i]!='*'):
+                summary += report[i]
+                i+=1
+
+            summary += "Risk: " + risk + "\n"
+            summary += str(violation_count[cnt]) + " time(s)\n"
+            summary +="</div>"
+
+            summary_list.append(summary)
+            summary = ""
+
+            cnt+=1
+
+        score = e ** non_standardised_score
+
+        # score = (e ** non_standardised_score) / ((e ** non_standardised_score)+1)
+        # score *= 100
+
+        summary = "<pre style=\"font-family: Garamond, serif;\">"
+        summary += "Total violations: " + str(total_violations) + "\n"
+        # summary += "Cryptoguard Score: " + str(score) + "%\n\n"
+        i = 0
+        for elem in summary_list:
+            summary += elem
+            if i!=len(summary_list)-1:
+                summary += "\n"
+            i+=1
+        summary+="</pre>"
+
+        return summary
+
+        # return res, violation_count, summary_list
+
+    def __init__(self):
+        print("")
--- playground/views.py
+++ playground/views.py
@@ -8,8 +8,7 @@ from django.shortcuts import redirect
 import subprocess
 import os
 import shutil
-from django import forms
-import re
+from playground.crypto_logic.cryptoguard import Cryptoguard
 
 name = 'hello'
 cogni_check = False
@@ -19,10 +18,7 @@ file_name = ''
 cryptoguard_report_file_name = ''
 security_analysis_check = True
 
-def run_source():
-    os.environ['ANDROID_HOME'] = '/home/ahmedulkavi/Android/Sdk'
-    os.environ['JAVA7_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
-    os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
+cryptoguard = Cryptoguard()
 
 def run_cognicrypt():
     global file_path
@@ -39,121 +35,6 @@ def run_cognicrypt():
     # temp = subprocess.Popen([cmd], stdout = subprocess.PIPE)
     # subprocess.Popen(["cd", android_sdk_platform], stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).communicate()
     # output = subprocess.Popen(["ls", android_sdk_platform], stdout = subprocess.PIPE).communicate()
-
-def run_cryptoguard():
-    global file_path
-
-    cmd = "java"
-
-    cryptoguard_path = 'crypto_tools/cryptoguard.jar'
-    apk_path = file_path
-
-    run_source()
-
-    temp = subprocess.Popen([cmd, '-jar', cryptoguard_path, '-in', 'apk', '-s', apk_path, '-m', 'L'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    output = temp.communicate()
-    # output = temp.args
-    # output = subprocess.list2cmdline(output)
-
-    return output
-
-
-def check_cryptoguard_violations(report):
-    low_weight = 1
-    medium_weight = 2
-    high_weight = 3
-
-    non_standardised_score = 0
-
-    total_violations = report.count('***Violated Rule ')
-
-    res = [m.start() for m in re.finditer(re.escape('***Violated Rule '), report)]
-    violation_count = []
-    violation_rule_number = []
-    summary_list = []
-    summary = ""
-    # report.count('***Violated Rule '),
-    i = 0
-    count = 0
-    for elem in res:
-        if i+1<len(res):
-            count = report.count('***Found:', elem, res[i+1])
-        else:
-            count = report.count('***Found:', elem)
-
-        violation_count.append(count)
-        i+=1
-        
-    for i in range(0, len(violation_count)-1):
-        if violation_count[i] == 0:
-            violation_count[i] = 1
-
-    for elem in res:
-        i = elem + 17
-        num = ""
-        while(report[i]!=':'):
-            num += report[i]
-            i+=1
-
-        num = int(num)
-        violation_rule_number.append(num)
-
-    cnt = 0
-    for elem in res:
-        risk = ''
-        if (violation_rule_number[cnt]>=1 and violation_rule_number[cnt]<=7) or violation_rule_number[cnt]==16:
-            risk = "High"
-            non_standardised_score += high_weight * violation_count[cnt]
-
-        elif violation_rule_number[cnt]>=8 and violation_rule_number[cnt]<=12:
-            risk = "Medium"
-            non_standardised_score += medium_weight * violation_count[cnt]
-
-        else:
-            risk = "Low"
-            non_standardised_score += low_weight * violation_count[cnt]
-
-        if risk=="High":
-            summary += "<div style=\"color:red;\">"
-        elif risk=="Medium":
-            summary += "<div style=\"color:orange;\">"
-        else:
-            summary += "<div style=\"color:black;\">"
-
-        i = elem + 3
-        while(report[i]!='*'):
-            summary += report[i]
-            i+=1
-
-        summary += "Risk: " + risk + "\n"
-        summary += str(violation_count[cnt]) + " time(s)\n"
-        summary +="</div>"
-
-        summary_list.append(summary)
-        summary = ""
-
-        cnt+=1
-
-    score = e ** non_standardised_score
-
-    # score = (e ** non_standardised_score) / ((e ** non_standardised_score)+1)
-    # score *= 100
-
-    summary = "<pre style=\"font-family: Garamond, serif;\">"
-    summary += "Total violations: " + str(total_violations) + "\n"
-    # summary += "Cryptoguard Score: " + str(score) + "%\n\n"
-    i = 0
-    for elem in summary_list:
-        summary += elem
-        if i!=len(summary_list)-1:
-            summary += "\n"
-        i+=1
-    summary+="</pre>"
-
-    return summary
-
-    # return res, violation_count, summary_list
-
 
 # Create your views here.
 def say_hello(request):
@@ -197,7 +78,7 @@ def select_tool(request):
     return render(request, 'selector.html')
 
 def result (request):
-    global crypto_check, cogni_check, file_name, cryptoguard_report_file_name
+    global crypto_check, cogni_check, file_name, cryptoguard_report_file_name, cryptoguard
 
     cryptoguard_report = ''
     cognicrypt_report = ''
@@ -225,8 +106,6 @@ def result (request):
         
         f.close()
 
-        print(check_cryptoguard_violations(cryptoguard_report))
-
     if cogni_check:
         path = 'cognicrypt-reports/'
         path += file_name.replace(".apk", "")
@@ -239,7 +118,7 @@ def result (request):
         
         f.close()
 
-    cryptoguard_summary = check_cryptoguard_violations(cryptoguard_report)
+    cryptoguard_summary = cryptoguard.check_cryptoguard_violations(cryptoguard_report)
 
     context = {
         'cryptoguard_report': cryptoguard_report,
@@ -259,8 +138,9 @@ def result (request):
     return render(request, 'result.html', context)
 
 def process(request):
+    global file_path, cryptoguard
     if crypto_check:
-        run_cryptoguard()
+        cryptoguard.run_cryptoguard(file_path)
 
     if cogni_check:
         run_cognicrypt()
